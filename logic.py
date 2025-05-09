2b25c7973faac2ee201f79c52381a022eb88f364
eli-nova-222 <eliasseranova@gmail.com>
2025-05-09 05:51:47 -0500

    figuring out logic and exceptions

--- logic.py
+++ logic.py
@@ -9,15 +9,30 @@ class Logic(QMainWindow, Ui_MainWindow):
         self.button_submit.clicked.connect(lambda: self.submit())
 
 
+
+
     def submit(self):
         try:
          id_num = int(self.input_id.text())
+         self.cast_vote()
          print(id_num)
          self.input_id.setText('')
 
         except ValueError:
             self.label_result.setText(f'ID must only use digits [0-9]')
+            self.input_id.setText('')
+        except Warning:
+            self.label_result.setText(f'Please select candidate')
+            self.input_id.setText('')
 
-
+    def cast_vote(self):
+        if self.radio_bianca.isChecked():
+            print('bianca')
+        elif self.radio_edward.isChecked():
+            print('edward')
+        elif self.radio_felicia.isChecked():
+            print('felicia')
+        else:
+            raise Warning
 
 
