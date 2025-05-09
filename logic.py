44a6703f8772ebe8f893af3c4f87d95259c71b97
eli-nova-222 <eliasseranova@gmail.com>
2025-05-09 05:22:27 -0500

    figuring out logic

--- logic.py
+++ logic.py
@@ -2,20 +2,21 @@ from PyQt6.QtWidgets import *
 from gui import *
 
 class Logic(QMainWindow, Ui_MainWindow):
-	def __init__(self):
-		super().__init__()
-		self.setupUi(self)
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
 
-		self.button_submit.clicked.connect(lambda: self.submit())
+        self.button_submit.clicked.connect(lambda: self.submit())
 
 
-	def submit(self):
-		try:
-			id_num = int(self.input_id.text())
-			print(id_num)
+    def submit(self):
+        try:
+         id_num = int(self.input_id.text())
+         print(id_num)
+         self.input_id.setText('')
 
-		except ValueError:
-			self.label_result.setText(f'ID must only use digits [0-9]')
+        except ValueError:
+            self.label_result.setText(f'ID must only use digits [0-9]')
 
 
 
